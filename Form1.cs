b1ac038e56b22a0e81100fd1ecbe9050b37fca28
Ash Fischer <afisc38@wgu.edu>
2023-08-07 11:45:05 -0500

    Adding connection to local Database

--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace software_2_c969
 {
@@ -16,5 +18,32 @@ namespace software_2_c969
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+			string constr = ConfigurationManager.ConnectionStrings["localdb"].ConnectionString;
+
+			MySqlConnection conn = null;
+
+			try
+            {
+				conn = new MySqlConnection(constr);
+
+				conn.Open();
+
+				MessageBox.Show("Connection is open.");
+            }
+			catch(MySqlException ex)
+            {
+				MessageBox.Show(ex.Message);
+            }
+			finally
+            {
+				if(conn != null)
+                {
+					conn.Close();
+				}
+            }
+        }
+    }
 }
